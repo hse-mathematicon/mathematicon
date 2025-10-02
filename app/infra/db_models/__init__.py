120abfc0c806f38287d1bb975041c7b851e04326
Виктория Князькова <vknyazkova01@gmail.com>
2025-10-02 13:45:27 +0300

    add transcripts table

--- app/infra/db_models/__init__.py
+++ app/infra/db_models/__init__.py
@@ -1,1 +1,2 @@
 from app.infra.db_models.base import BaseModel
+from app.infra.db_models.transcripts import TranscriptDBModel
---
+++ app/infra/db_models/transcripts.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.infra.db_models.base import BaseModel
+
+
+class TranscriptDBModel(BaseModel):
+    __tablename__ = "transcripts"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    filename: Mapped[str] = mapped_column(unique=True)
+    src_url: Mapped[str] = mapped_column(unique=True)
+    text: Mapped[str] = mapped_column(Text, deferred=True)
+    title: Mapped[str] = mapped_column()
+    timecode_start: Mapped[str] = mapped_column()
+    timecode_end: Mapped[str] = mapped_column()
+    math_branch: Mapped[str] = mapped_column()
+    difficulty: Mapped[str] = mapped_column()
