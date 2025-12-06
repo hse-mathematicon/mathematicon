f36418e316274e4b2dc3c2b83d4165d4dd1e6bac
Виктория Князькова <vknyazkova01@gmail.com>
2025-12-07 02:00:42 +0300

    upd db_path setting

--- app/settings/db.py
+++ app/settings/db.py
@@ -6,10 +6,10 @@ from app.variables import PROJECT_DIR
 
 
 class DBSettings(BaseSettings):
-    db_path: str
+    file_path: str
 
     @property
     def url(self) -> str:
-        return f"sqlite:///{Path(PROJECT_DIR, self.db_path).resolve()}"
+        return f"sqlite:///{Path(PROJECT_DIR, self.file_path).resolve()}"
 
     model_config = SettingsConfigDict(env_prefix="db_")
